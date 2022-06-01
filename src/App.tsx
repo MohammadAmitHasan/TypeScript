e676d5413c5ede37e8c8bda455a0b3efcf2a2d39
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-06-01 10:14:47 +0600

    Initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let myName: string = 'Muhammad Hasan';
+let myAge: number = 26;
+let ages: number[] = [78, 66, 52, 42, 37, 35];
+let names: string [] = ['Mizan', 'Papia', 'Aklima', 'Masaud', 'Masuma'];
+
+let person1: {name: string, age: number, weight: number} = {
+  name: 'Muhammad Hasan',
+  age: 26,
+  weight: 58
+}
+
+// Object declaration using interface
+interface Person2{
+  name: string,
+  age: number,
+  weight: number,
+  height?: number, // ? makes it optional
+  employed: string | boolean, // This is union in TS.
+  location? : any, // Means it can be anything
+}
+let person2:Person2  = {
+  name: 'Muhammad Hasan',
+  age: 26,
+  weight: 58,
+  employed: true,
+}
+
+// Object declaration using type
+type School ={
+  name: string,
+  students: number,
+}
+let school1 : School ={
+  name: 'Model',
+  students: 30000,
+}
+
+const sayName = (name: string, times: number):string =>{
+  // void means no return
+  const say = name + times;
+  return say;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,3 @@
+let tesVar = 'Hasan';
+tesVar = 78; // The type of the variable is changed but it works fine
+console.log(tesVar);
